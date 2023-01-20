2220f759ca7673cf61626626d0637077c54f8c7f
Michał Flak <michal.flak.96@gmail.com>
2023-01-20 03:29:28 +0100

    Sprobuj zrobic jak tu, tylko dodaj pliki do projektu (najlepiej stworz je od nowa w VS i skopiuj zawartosc)

---
+++ Forum/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Forum.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IForumService _forumService;
+        public HomeController(IForumService forumService)
+        {
+            _forumService = forumService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var posts = _forumService.GetAll();
+            return View(posts);
+        }
+    }
+}
+
